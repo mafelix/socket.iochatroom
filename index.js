fb0ccfb6c833f1583a662fdb0e9176742a0f6d92
felix <felixmdev@gmail.com>
2016-04-15 18:05:43 -0700

    added users, connect, disconnect, better understanding of socket.io

--- index.js
+++ index.js
@@ -13,23 +13,27 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('a user has disconnected');
   });
-  socket.on('chat_message', function(msg){
-    console.log('message: ' + msg);
-  });
-  socket.on('chat_message', function(msg){
-    io.sockets.emit('chat_message', msg);
-  });
-
-
+ 
   socket.on('adduser', function(name){
     socket.name = name
     usernames[name] = name
 
-    socket.broadcast.emit('updatechat', socket.name + 'has connected');
+    socket.broadcast.emit('updatechat', socket.name + ' has connected');
     io.emit('updateusers', usernames);
     console.log(usernames);
   })
+  socket.on('chat_message', function(msg){
+    io.sockets.emit('chat_message', socket.name + ": " + msg);
+  })
 
+  socket.on('disconnect', function(){
+    // deleting socket property name from usernames object
+    delete usernames[socket.name];
+    // updating ALL sockets about new users list/ passing it usernames object
+    io.sockets.emit('updateusers', usernames);
+    // updating chat box with message of disconnected user
+    socket.broadcast.emit('updatechat', socket.name + " has disconnected");
+  });
 });
 
 http.listen(3000, function(){
